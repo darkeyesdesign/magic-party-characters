9944dfadc6b09b7afdd63edf3e6ed16c3f8c4799
billbunkum <20425559+billbunkum@users.noreply.github.com>
2026-05-27 17:19:44 -0700

    create deploy.yml workflow; edit next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const repo = 'magic-party-characters'
+
 const nextConfig = {
   reactStrictMode: true,
 	output: 'export',
@@ -6,6 +8,9 @@ const nextConfig = {
 		unoptimized: true,
 	},
 	generateEtags: false, // prevents Github from thinking nothing has changed
+
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}`,
 }
 
 /* I clearly have to do something just so GitHub will fucking work */
